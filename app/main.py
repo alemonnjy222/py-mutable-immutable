da9e8ce4724e552317b67fc8f31b3fb84e0a00c3
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-02-13 07:35:02 +0100

    Update2 main.py

--- app/main.py
+++ app/main.py
@@ -17,5 +17,5 @@ marks = {
 collection_of_coins = {1, 2, 25} #set
 
 sorted_variables = {"mutable":
-                    [my_favorite_films, marks, collection_of_coins],
+                    [my_favourite_films, marks, collection_of_coins],
                     "immutable":[lucky_number, pi, one_is_a_prime_number, name, profile_info]}
